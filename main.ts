852af43627b1e0998944466ce08442596bda45a4
Neighborhood Makers <53877723+neighborhood-makers@users.noreply.github.com>
2023-04-01 11:13:35 -0400

    make the cross jump around

--- main.ts
+++ main.ts
@@ -1,10 +1,48 @@
-basic.showLeds(`
-    . . # . .
-    . # # # .
-    . . # . .
-    . . # . .
-    . . # . .
-    `)
 basic.forever(function () {
 	
+})
+loops.everyInterval(100, function () {
+    basic.showLeds(`
+        . . # # #
+        . . . # .
+        . . . # .
+        . . . # .
+        . . . . .
+        `)
+})
+loops.everyInterval(100, function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+})
+loops.everyInterval(100, function () {
+    basic.showLeds(`
+        . # . . .
+        # # # . .
+        . # . . .
+        . # . . .
+        . # . . .
+        `)
+})
+loops.everyInterval(100, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . . .
+        # # # . .
+        . # . . .
+        . # . . .
+        `)
+})
+loops.everyInterval(100, function () {
+    basic.showLeds(`
+        . . . # .
+        . . # # #
+        . . . # .
+        . . . # .
+        . . . # .
+        `)
 })
